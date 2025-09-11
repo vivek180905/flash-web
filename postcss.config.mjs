2f360bb55b93307594a7c29ecaf688bffdb0ba67
Vivek Raj <viraj9999singh@gmail.com>
2025-09-11 15:44:19 +0530

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,7 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-import scrollbarHide from "tailwind-scrollbar-hide";
-
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-    scrollbarHide,
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    "tailwind-scrollbar-hide": {},
+  },
 };
